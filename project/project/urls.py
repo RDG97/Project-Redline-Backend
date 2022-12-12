3e48cfd910a82e7610cd921aa46118defaeb602a
RDG97 <ryangann1997@gmail.com>
2022-12-12 17:56:57 +0000

    Readying for deployment

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,3 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('redline.urls')),
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
